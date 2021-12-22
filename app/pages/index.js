5bccb2b55c6c718ace0ef186f5d46778f221136e
AR <74219764+AR-Student824@users.noreply.github.com>
2021-12-21 16:15:48 -0800

    ;

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Typed from 'typed.js'
+import {useEffect} from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    var typed = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+      startdelay: 1000,
+      typeSpeed: 100,
+      backDelay: 300,
+      backSpeed: 100
+    });
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -14,8 +25,19 @@ export default function Home() {
       <main className={styles.main}>
         <center>
           <h1 className={styles.title}>
-          Introducing <span className="pink">Devneo</span>.
+          A place for <span className="pink" id="typed">developers</span>.
         </h1>
+
+        <div id="typed-strings" style={{display: "none"}}>
+          <h1>developers</h1>
+          <h1>code snippets</h1>
+          <h1>projects</h1>
+          <h1>ideas</h1>
+          <h1>jobs</h1>
+          <h1>APIs</h1>
+          <h1>developers</h1>
+
+        </div>
 
         <p className={styles.description}>
           Share and find code snippets, projects and ideas, all in one place.
