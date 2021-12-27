83298f23812d1cfeaa01e1794d6617897be43057
AR <74219764+AR-Student824@users.noreply.github.com>
2021-12-26 18:06:27 -0800

    ;

--- app/pages/components/sidebar.js
+++ app/pages/components/sidebar.js
@@ -4,11 +4,11 @@ class Sidebar extends Component {
   render() {
     return (
         <>
-            <div class="w3-sidebar w3-bar-block" style={{width:"25%"}}>
-            <h3 class="w3-bar-item">Menu</h3>
-  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+            <div className="w3-sidebar w3-bar-block" style={{width:"25%"}}>
+            <h3 className="w3-bar-item">Menu</h3>
+  <a href="#" className="w3-bar-item w3-button">Link 1</a>
+  <a href="#" className="w3-bar-item w3-button">Link 2</a>
+  <a href="#" className="w3-bar-item w3-button">Link 3</a>
 </div>
         </>
     )
