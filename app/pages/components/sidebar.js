b459dc1f9391c7602986cb43154a0cd3c014262a
AR <74219764+AR-Student824@users.noreply.github.com>
2021-12-28 14:13:49 -0800

    ['

--- app/pages/components/sidebar.js
+++ app/pages/components/sidebar.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 
 class Sidebar extends Component {
-  render(signout) {
+  render(signout, username, avatarUrl) {
     return (
         <>
             <div className="w3-sidebar w3-bar-block" style={{width:"25%"}}>
@@ -11,7 +11,7 @@ class Sidebar extends Component {
   <a onClick={this.props.signout} className="w3-bar-item w3-button">👋 Sign Out</a>
 
   <div className="placeBottom">
-  <h3>Logged in as <Image src={session.user.image} alt="avatar" className={styles.avatar} width="25px" height="25px" /> {session.user.name}</h3>
+  <h3>Logged in as <Image src={this.props.username} alt="avatar" className={styles.avatar} width="25px" height="25px" /> {this.props.avatarUrl}</h3>
   </div>
 </div>
         </>
