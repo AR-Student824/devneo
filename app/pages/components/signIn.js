88f9d7ddc565ba67c1130a9235f78940cb985247
ar <74219764+AR-Student824@users.noreply.github.com>
2021-12-29 18:25:12 -0800

    ;

--- app/pages/components/signIn.js
+++ app/pages/components/signIn.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Image from "next/image"
 import styles from '../../styles/Home.module.css'
+import Button from '@mui/material/Button'
 
 class SignIn extends Component {
   render(signInFunction) {
